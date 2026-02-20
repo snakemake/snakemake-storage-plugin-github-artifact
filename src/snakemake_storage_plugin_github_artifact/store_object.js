5bd48cefd46b74778557ca349c0a788f0f624376
Johannes Koester <johannes.koester@uni-due.de>
2026-02-20 14:08:45 +0100

    fix

--- src/snakemake_storage_plugin_github_artifact/store_object.js
+++ src/snakemake_storage_plugin_github_artifact/store_object.js
@@ -4,4 +4,4 @@ const artifactClient = new DefaultArtifactClient();
 
 let path = process.argv[2];
 let name = process.argv[3];
-await artifactClient.uploadArtifact(name, [path], ".");
+await artifactClient.uploadArtifact(name, [path]);
